38180d4aba99fb7a4b93d8bbaaf09f442065fb1d
Brian Lauber <constructible.truth@gmail.com>
2016-02-17 00:20:03 -0500

    Bug fix: No longer renders Array.length fields

--- src/index.js
+++ src/index.js
@@ -5,29 +5,50 @@ const renderers = require('./path_renderers');
 function flatten(obj, options) {
   options = options || {};
 
-  const retval = {};
   const getProperties = options.getProperties || Object.getOwnPropertyNames;
   const render = options.renderer || renderers.simple;
 
-  function __flatten(o, parent) {
-    const keys = getProperties(o);
-    for(let i = 0; i < keys.length; ++i) {
-      const key = keys[i];
-      const value = o[key];
 
+  const retval = {};
+
+  function __flatten(o, fragment) {
+    if(Array.isArray(o)) {
+      __flattenArray(o, fragment);
+    } else if((typeof o) === 'object') {
+      __flattenObject(o, fragment);
+    } else {
+      const key = render(fragment);
+      retval[key] = o;
+    }
+  }
+
+  function __flattenArray(array, parent) {
+    for(let i = 0; i < array.length; ++i) {
+      const o = array[i];
       const fragment = {
-        value: key,
+        value: i,
         parent: parent
       };
 
-      if((typeof value) === 'object') {
-        __flatten(value, fragment);
-      } else {
-        retval[render(fragment)] = value;
-      }
-
+      __flatten(o, fragment);
     }
   }
+
+  function __flattenObject(obj, parent) {
+    const keys = getProperties(obj);
+    for(let i = 0; i < keys.length; ++i) {
+      const key = keys[i];
+      const value = obj[key];
+      const fragment = {
+        value: key,
+        parent: parent
+      }
+
+      __flatten(value, fragment);
+    }
+  }
+
+
 
   __flatten(obj);
 
